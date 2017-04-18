8a2e14d268d11e3a2e772618f73e437ee84a2534
Kenneth Onah <kenneth@netcraft-devops.com>
2017-04-18 14:59:43 +0200

    added api context for tokenized payments

--- samples/bootstrap.php
+++ samples/bootstrap.php
@@ -66,6 +66,12 @@ $acct6Safekey = '{07F70723-1B96-4B97-B891-7BF708594EEA}';
 $acct6StoreId = 'Staging Integration Store 3';
 $acct6PaymentMethods = 'EFT_PRO';
 
+$acct7Username = '200239';
+$acct7Password = '5AlTRPoD';
+$acct7Safekey = '{07F70723-1B96-4B97-B891-7BF708594EEA}';
+$acct7StoreId = 'Staging Integration Store';
+$acct7PaymentMethods = 'CREDITCARD';
+
 
 /**
  * All default SOAP options are stored in the array inside the Http\Config class. To make changes to those settings
@@ -80,22 +86,26 @@ $apiContext = getApiContext(
     array(
         $acct1Username, $acct2Username,
         $acct3Username, $acct4Username,
-        $acct5Username, $acct6Username
+        $acct5Username, $acct6Username,
+        $acct7Username,
     ),
     array(
         $acct1Password, $acct2Password,
         $acct3Password, $acct4Password,
-        $acct5Password, $acct6Password
+        $acct5Password, $acct6Password,
+        $acct7Password,
     ),
     array(
         $acct1Safekey, $acct2Safekey,
         $acct3Safekey, $acct4Safekey,
-        $acct5Safekey, $acct6Safekey
+        $acct5Safekey, $acct6Safekey,
+        $acct7Safekey,
     ),
     array(
         $acct1PaymentMethods, $acct2PaymentMethods,
         $acct3PaymentMethods, $acct4PaymentMethods,
-        $acct5PaymentMethods, $acct6PaymentMethods
+        $acct5PaymentMethods, $acct6PaymentMethods,
+        $acct7PaymentMethods,
     )
 );
 
@@ -169,6 +179,14 @@ function getApiContext($usernames, $passwords, $safekeys, $paymentMethods)
             $usernames[5],
             $passwords[5],
             $safekeys[5]
+        )
+    );
+
+    $apiContextCCToken = new ApiContext(
+        new BasicAuth(
+            $usernames[6],
+            $passwords[6],
+            $safekeys[6]
         )
     );
 
@@ -254,9 +272,23 @@ function getApiContext($usernames, $passwords, $safekeys, $paymentMethods)
         )
     );
 
+    $apiContextCCToken->setConfig(
+        array(
+            'mode' => 'sandbox',
+            'log.log_enabled' => true,
+            'log.file_name' => '../PayU.log',
+            'log.log_level' => 'DEBUG', // PLEASE USE `INFO` LEVEL FOR LOGGING IN LIVE ENVIRONMENTS
+            'cache.enabled' => true,
+            'acct6.payment_methods' => $paymentMethods[6]
+            //'http.connect_timeout' => 30
+            //'log.AdapterFactory' => '\PayU\Log\DefaultLogFactory' // Factory class implementing \PayU\Log\PayULogFactory
+        )
+    );
+
     return array(
         $apiContextEnterprise, $apiContextRedirect,
         $apiContextFm, $apiContextDebitOrder,
-        $apiContextRTR, $apiContextEFTRedirect
+        $apiContextRTR, $apiContextEFTRedirect,
+        $apiContextCCToken
     );
 }
